8778bd5a96e45be6ce9f9b6ea4f05f5e232ef653
Mayank Jain <mayankjain1784@gmail.com>
2023-09-15 12:24:03 +0530

    made node backend with cors handling

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+
+app.use("/books",booksRoute);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const port = process.env.PORT || 8000;
